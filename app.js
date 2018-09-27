dacf1968b4880d66316cf407b57f9daac2e31db6
A. Ibrahim <ahmedmokhtar75@gmail.com>
2018-09-27 19:55:34 +0200

    Finished n-4-6

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAYwDeUT6Rs5uDCmYF6Mm2E8z5hhPuHTSc',
     json: true
 }, (error, response, body) => {
-    console.log(body)
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${body.results[0].geometry.location.lng}`)
 })
