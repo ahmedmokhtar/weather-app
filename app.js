f889dca54914b7a6f8f0742a682f03f0e6d1c333
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-09-27 23:34:52 +0200

    Finished n-4-7

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'The address to fetch the weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('h', 'help')
+    .argv
+
+    const encodedAddress = encodeURIComponent(argv.address)
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAYwDeUT6Rs5uDCmYF6Mm2E8z5hhPuHTSc',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyAYwDeUT6Rs5uDCmYF6Mm2E8z5hhPuHTSc`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${body.results[0].geometry.location.lng}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}\nLongitude: ${body.results[0].geometry.location.lng}`)
 })
