def1f942fa4d76964d840531e302489f267cded8
A. Ibrahim <ahmedmokhtar75@gmail.com>
2018-09-28 18:52:04 +0200

    Finished n-4-8

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyAYwDeUT6Rs5uDCmYF6Mm2E8z5hhPuHTSc`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}\nLongitude: ${body.results[0].geometry.location.lng}`)
+
+    if (error) {
+        console.log('Unable to connect to Google servers.')
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.')
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`)
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}\nLongitude: ${body.results[0].geometry.location.lng}`)    
+    }
 })
